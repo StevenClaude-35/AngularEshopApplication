337961ce73a084d582c8f66d34c10bd4b17a48be
claude cavour <aristide22lapo@gmail.com>
2021-06-30 16:53:47 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { CartComponent } from './components/shop/cart/cart.component';
+import { EditProductComponent } from './components/shop/edit-product/edit-product.component';
+import { SingleProductComponent } from './components/shop/single-product/single-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'signup',component:SignupComponent},
+{path:'signin',component:SigninComponent},
+{path:'shop',component:SignupComponent},
+{path:'add-product',component:SignupComponent},
+{path:'single-product/:id',component:SingleProductComponent},
+{path:'edit-poduct/:id',component:EditProductComponent},
+{path:'cart',component:CartComponent},
+{path:'**',component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
